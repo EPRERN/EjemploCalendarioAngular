6d5449b0a054ab56ada3a8b66718689f3f1762db
Lautaro Avila <lavila@eprern.gov.ar>
2024-05-06 14:56:20 -0300

    segundo commit

--- src/app/Components/calendar/Services/calendar.service.ts
+++ src/app/Components/calendar/Services/calendar.service.ts
@@ -4,6 +4,9 @@ import { Observable, catchError } from 'rxjs';
 import { CalendarDay, CalendarEvent } from '../Models';
 import { getDaysForMonthPage } from '../../Data/Data';
 
+
+
+
 @Injectable({
   providedIn: 'root',
 })
@@ -42,11 +45,10 @@ export class CalendarService {
     return getDaysForMonthPage(date);
   }
 
-  // Modifica el método deleteAllEventsOfTheDay para enviar la fecha en el cuerpo de la solicitud
+
   public deleteAllEventsOfTheDay(date: Date): Observable<any> {
-    // Crea un objeto HttpParams para enviar la fecha en el cuerpo de la solicitud
+    
     const params = new HttpParams().set('date', date.toISOString());
-    // Utiliza los parámetros de la solicitud para enviar la fecha
     return this.http.delete<any>(`${this.baseUrl}/deleteAllEventsOfTheDay`, { params });
   }
 }
